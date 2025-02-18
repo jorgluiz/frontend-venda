1f66bfb500bf79d0f60692cbc818eecebb934a10
jorgluiz <jorgeluizti@hotmail.com>
2025-02-18 04:48:44 -0300

    fix: update heading text for non-subscriber route

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -2,11 +2,11 @@ import { StyledContainer } from "./styles";
 
 const Home = () => {
 
-   return (
-      <StyledContainer>
-         <h1 style={{ textAlign: "center" }}>aqui fica aba para usuário não pagante tem acesso.</h1>
-      </StyledContainer>
-   );
+  return (
+    <StyledContainer>
+      <h1 style={{ textAlign: "center" }}>rota para usuário não assinante</h1>
+    </StyledContainer>
+  );
 };
 
 export default Home;
